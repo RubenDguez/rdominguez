b8d9d655f425121d5e30bfcfd7a8514a99194a09
Ruben Dominguez <71775213+RubenDguez@users.noreply.github.com>
2022-08-12 22:06:23 -0400

    enforcing linting and formatting

--- src/components/UI/LeftPanel.tsx
+++ src/components/UI/LeftPanel.tsx
@@ -1,19 +1,19 @@
 import {
-    Box,
-    Card,
-    CardContent,
-    CardMedia,
-    Divider,
-    Grid,
-    Typography,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Typography,
 } from '@mui/material';
 import { Container } from '@mui/system';
 import {
-    TImage,
-    TMediaLink,
-    TPersonalInformation,
-    TSkill,
-    TSpokenLanguage,
+  TImage,
+  TMediaLink,
+  TPersonalInformation,
+  TSkill,
+  TSpokenLanguage,
 } from '../../types';
 import { MediaLinks } from './MediaLinks';
 import { PersonalInf } from './PersonalInformation';
@@ -35,114 +35,114 @@ export interface ILeftPanel {
 }
 
 export const LeftPanel = ({
-    name,
-    position,
-    personalInformationData,
-    spokenLanguages,
-    frontEndSkills,
-    backEndSkills,
-    skills,
-    mediaLinks,
-    image,
+  name,
+  position,
+  personalInformationData,
+  spokenLanguages,
+  frontEndSkills,
+  backEndSkills,
+  skills,
+  mediaLinks,
+  image,
 }: ILeftPanel) => {
-    return (
-        <Card>
-            <Box
-                sx={{
-                    padding: '1rem',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100%',
-                    height: '100%',
-                }}
-            >
-                <CardMedia
-                    sx={{
-                        borderRadius: '50%',
-                        height: 160,
-                        width: 160,
-                    }}
-                    component='img'
-                    alt='ruben dominguez'
-                    image={image}
-                />
-            </Box>
-            <Box sx={{ width: '100%', textAlign: 'center' }}>
-                <Typography variant='h6'>{name}</Typography>
-                <Typography>{position}</Typography>
-            </Box>
-            <CardContent>
-                <Divider />
-                {/* begin personal information */}
-                <Container sx={{ my: 2 }}>
-                    <PersonalInf personalInformationData={personalInformationData} />
-                </Container>
-                {/* end personal information */}
+  return (
+    <Card>
+      <Box
+        sx={{
+          padding: '1rem',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <CardMedia
+          sx={{
+            borderRadius: '50%',
+            height: 160,
+            width: 160,
+          }}
+          component="img"
+          alt="ruben dominguez"
+          image={image}
+        />
+      </Box>
+      <Box sx={{ width: '100%', textAlign: 'center' }}>
+        <Typography variant="h6">{name}</Typography>
+        <Typography>{position}</Typography>
+      </Box>
+      <CardContent>
+        <Divider />
+        {/* begin personal information */}
+        <Container sx={{ my: 2 }}>
+          <PersonalInf personalInformationData={personalInformationData} />
+        </Container>
+        {/* end personal information */}
 
-                {/* begin spoken languages */}
-                <Divider />
-                <Container sx={{ my: 2 }}>
-                    <Grid
-                        container
-                        spacing={2}
-                        sx={{
-                            display: 'flex',
-                            alignContent: 'center',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <SpokenLanguages spokenLanguages={spokenLanguages} />
-                    </Grid>
-                </Container>
-                {/* end spoken languages */}
+        {/* begin spoken languages */}
+        <Divider />
+        <Container sx={{ my: 2 }}>
+          <Grid
+            container
+            spacing={2}
+            sx={{
+              display: 'flex',
+              alignContent: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <SpokenLanguages spokenLanguages={spokenLanguages} />
+          </Grid>
+        </Container>
+        {/* end spoken languages */}
 
-                {/* begin front end skills */}
-                <Divider />
-                <Box
-                    sx={{
-                        textAlign: 'center',
-                        marginTop: '1rem',
-                        textTransform: 'uppercase',
-                    }}
-                >
-                    <Typography>Front End Skills</Typography>
-                </Box>
-                <Container sx={{ my: 2 }}>
-                    <SkillsWithProgressBar skills={frontEndSkills} />
-                </Container>
-                {/* end front end skills */}
+        {/* begin front end skills */}
+        <Divider />
+        <Box
+          sx={{
+            textAlign: 'center',
+            marginTop: '1rem',
+            textTransform: 'uppercase',
+          }}
+        >
+          <Typography>Front End Skills</Typography>
+        </Box>
+        <Container sx={{ my: 2 }}>
+          <SkillsWithProgressBar skills={frontEndSkills} />
+        </Container>
+        {/* end front end skills */}
 
-                {/* begin back end skills */}
-                <Divider />
-                <Box
-                    sx={{
-                        textAlign: 'center',
-                        marginTop: '1rem',
-                        textTransform: 'uppercase',
-                    }}
-                >
-                    <Typography>Back End Skills</Typography>
-                </Box>
-                <Container sx={{ my: 2 }}>
-                    <SkillsWithProgressBar skills={backEndSkills} />
-                </Container>
-                {/* end back end skills */}
+        {/* begin back end skills */}
+        <Divider />
+        <Box
+          sx={{
+            textAlign: 'center',
+            marginTop: '1rem',
+            textTransform: 'uppercase',
+          }}
+        >
+          <Typography>Back End Skills</Typography>
+        </Box>
+        <Container sx={{ my: 2 }}>
+          <SkillsWithProgressBar skills={backEndSkills} />
+        </Container>
+        {/* end back end skills */}
 
-                {/* begin other skills */}
-                <Divider />
-                <Container sx={{ my: 2 }}>
-                    <Skills skills={skills} />
-                </Container>
-                {/* end other skills */}
+        {/* begin other skills */}
+        <Divider />
+        <Container sx={{ my: 2 }}>
+          <Skills skills={skills} />
+        </Container>
+        {/* end other skills */}
 
-                {/* begin media links */}
-                <Divider />
-                <Container sx={{ my: 2 }}>
-                    <MediaLinks mediaLinks={mediaLinks} />
-                </Container>
-                {/* end media links */}
-            </CardContent>
-        </Card>
-    );
+        {/* begin media links */}
+        <Divider />
+        <Container sx={{ my: 2 }}>
+          <MediaLinks mediaLinks={mediaLinks} />
+        </Container>
+        {/* end media links */}
+      </CardContent>
+    </Card>
+  );
 };
--- src/components/UI/SpokenLanguages.tsx
+++ src/components/UI/SpokenLanguages.tsx
@@ -7,27 +7,27 @@ export interface ISpokenLanguages {
 }
 
 export const SpokenLanguages = ({ spokenLanguages }: ISpokenLanguages) => {
-    return (
-        <>
-            {spokenLanguages.map((m, i) => (
-                <Grid key={i} item>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <CircularProgressBarWithLabel value={m.percentage} />
-                        <Box sx={{ py: 1 }}>
-                            <Typography variant="caption" sx={{ textTransform: 'uppercase' }}>
-                                {m.language}
-                            </Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {spokenLanguages.map((m, i) => (
+        <Grid key={i} item>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <CircularProgressBarWithLabel value={m.percentage} />
+            <Box sx={{ py: 1 }}>
+              <Typography variant="caption" sx={{ textTransform: 'uppercase' }}>
+                {m.language}
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      ))}
+    </>
+  );
 };
