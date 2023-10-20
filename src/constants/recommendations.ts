1a461ac0fc732ddb49378208197646b2ad5f677a
Ruben Dominguez <71775213+RubenDguez@users.noreply.github.com>
2023-10-20 12:00:11 -0400

    update: constants (#15)

--- src/constants/recommendations.ts
+++ src/constants/recommendations.ts
@@ -1,41 +1,19 @@
 import { TRecommendation } from '../components/UI/Recommendations';
-import alex from '../images/alex.png';
-import lisa from '../images/lisa.png';
-import subZero from '../images/subZero.jpeg';
-import scorpion from '../images/scorpion.jpeg';
 
 export const recommendations: TRecommendation[] = [
   {
-    name: 'Scorpion',
-    title: 'CEO',
-    comment:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quo molestiae saepe expedita officia nulla officiis aut. Aperiam aliquam cupiditate earum necessitatibus ipsam unde assumenda!',
-    imageSrc: scorpion,
+    name: 'Stephen Wu',
+    title: 'IT Technology Leader, NextEra Energy Solutions',
+    comment: `
+    Argenis has been a fantastic addition to the team and has been converted to FTE in June 2023. 
+    He has been a vital component to major initiatives this year such as leading the Salesforce CRM + LAND teams 
+    with standing up a regression suite. He is constantly helping and mentoring his peers with code design, 
+    reviews and feedback, and has helped our team to standardize modern reporting solutions like Allure. 
+    He is on track for continuing his efforts into building out production variant suites for Salesforce, 
+    as well as helping integrate the latest project PALMS into the platform. 
+    I am looking forward to watch him coninue to grow both his technical and professional skillset and cannot wait to see 
+    what exciting enhancements/ideas he will bring to the team.
+    `,
     review: 5,
-  },
-  {
-    name: 'Sub Zero',
-    title: 'Supervisor',
-    comment: 'Lorem ipsum dolor sit',
-    imageSrc: subZero,
-    review: 3,
-  },
-  {
-    name: 'Lisa Freiwald',
-    title: 'CEO',
-    comment: `Sunt commodo commodo nulla eiusmod. Reprehenderit qui qui ipsum ea tempor enim magna ipsum proident adipisicing proident Lorem. Pariatur labore exercitation sint elit sunt occaecat id consequat id irure deserunt nostrud. Enim velit sit magna consectetur est mollit et et et. Veniam deserunt est ad Lorem magna ut sint culpa adipisicing. Velit in esse duis sint non anim laborum non est pariatur commodo anim esse.
-
-Exercitation anim aliqua aliquip mollit aliquip est dolor anim id. Fugiat anim id laboris cupidatat. Sit eu quis occaecat deserunt pariatur eiusmod Lorem fugiat nisi cillum veniam Lorem. Excepteur ut officia voluptate ullamco ullamco voluptate mollit in elit ea laboris et Lorem officia. Commodo nisi pariatur incididunt et enim cupidatat. Cillum laborum ullamco elit veniam eiusmod labore quis irure.
-
-Laborum id magna aliquip sunt nulla quis nostrud occaecat voluptate ex. Dolore nulla enim irure sint officia nisi do cillum. Labore sint adipisicing in culpa sunt mollit occaecat incididunt et. Eiusmod aliqua culpa ea aliqua exercitation consectetur cupidatat qui pariatur adipisicing mollit. Labore velit eu excepteur quis reprehenderit quis anim do ipsum aliquip.`,
-    imageSrc: lisa,
-    review: 5,
-  },
-  {
-    name: 'Alexander Ramhit',
-    title: 'Supervisor',
-    comment: 'Lorem ipsum dolor sit',
-    imageSrc: alex,
-    review: 4,
   },
 ];
