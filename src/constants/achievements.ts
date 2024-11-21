135b1e66ae80475a5ca0ad1dd16f0bcdaed811ea
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-11-21 11:08:10 -0500

    update: achievements and recommendations

--- src/constants/achievements.ts
+++ src/constants/achievements.ts
@@ -3,7 +3,7 @@ import { recommendations } from './recommendations';
 
 export const achievements: TAchievement[] = [
   { achievement: 3, plus: true, name: 'Years of Experience' },
-  { achievement: 5, name: 'Completed Projects' },
+  { achievement: 13, name: 'Completed Projects' },
   { achievement: 2, name: 'Happy Customers' },
   { achievement: Number(`${recommendations.length}`), name: 'Recommendations' },
 ];
--- src/constants/recommendations.ts
+++ src/constants/recommendations.ts
@@ -11,8 +11,20 @@ export const recommendations: TRecommendation[] = [
     reviews and feedback, and has helped our team to standardize modern reporting solutions like Allure. 
     He is on track for continuing his efforts into building out production variant suites for Salesforce, 
     as well as helping integrate the latest project PALMS into the platform. 
-    I am looking forward to watch him coninue to grow both his technical and professional skillset and cannot wait to see 
+    I am looking forward to watch him continue to grow both his technical and professional skillets and cannot wait to see 
     what exciting enhancements/ideas he will bring to the team.
+    `,
+    review: 5,
+  },
+  {
+    name: 'Kenneth Hilgendorf',
+    title: 'IT Senior Manger, NextEra Energy Solutions',
+    comment: `
+    Ken would like to give a huge shout out to @Dominguez, Argenis for his recent updates on the NEER Salesforce regression suite
+    and DevOps solution. He took the initiative to redesign the architecture of our parallel execution, allowing us to take advantage
+    of the new limitations from the Salesforce release this year. This resulted in a massive optimization benefit for the teams
+    who run these 4+ times every week. CRM Full regression of 700+ tests cases went from taking ~2.5hours to ~1hour (60% reduction and improvement).
+    LAND full regression of 550+ test cases went from ~1h45m to 35m (65% reduction and improvement).
     `,
     review: 5,
   },
